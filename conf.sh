bef3b8f044d2bf3f12f6ebfd769f3f825062c0d4
kylerschin <kylerschin@gmail.com>
2016-02-18 12:40:17 -0800

    Create conf.sh

---
+++ conf.sh
@@ -0,0 +1,4 @@
+INVOCATION="java -Xmx${MAXHEAP}M -Xms${MINHEAP}M -XX:+UseConcMarkSweepGC \
+ -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=$CPU_COUNT -XX:+AggressiveOpts \
+ -jar $SERVICE $OPTIONS"
+#FUNCTIONS
