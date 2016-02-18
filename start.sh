0d86d0b1a7b87ccbf08865b2a4abf3caeb31d2e5
kylerschin <kylerschin@gmail.com>
2016-02-18 12:40:53 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,16 @@
 echo 'Reading Configuration File'
 ./conf.sh
 echo 'Done Reading Configuration File'
+echo 'Configuring Functions'
+ as_user() {
+   if [ "$ME" = "$USERNAME" ] ; then
+     bash -c "$1"
+   else
+     su - "$USERNAME" -c "$1"
+   fi
+ }
+  ME=`whoami`
+ echo 'Done Configurating Functions'
+ echo 'Starting the Big Guy...'
+ java -version
 start () { cd $DIR &&  }
