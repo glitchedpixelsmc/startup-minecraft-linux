073b44955d067ac78b2b19388028b0a2962fc607
kylerschin <kylerschin@gmail.com>
2016-02-21 20:52:46 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+if [ -f "conf.sh" ]
+then
+	echo "Hello World!"
+else
+	echo "MAXHEAP=1024
+MINHEAP=1024
+CPU_COUNT=1
+SERVICE='minecraft_server.jar'
+OPTIONS='nogui server'
+#put snapshot or release
+UPDATE='snapshot'
+ENABLEUPDATE='true'" >> conf.sh
+fi
 echo 'Reading Configuration File'
 ./conf.sh
 echo 'Done Reading Configuration File'
@@ -19,6 +32,6 @@ start () { echo 'Starting the server...'&& cd $DIR && java -Xmx${MAXHEAP}M -Xms$
  update () { echo 'fetching version data...'
  wget https://launchermeta.mojang.com/mc/game/version_manifest.json -O json.txt
  echo 'Reading Json data...'
- if [[$UPDATE]]
- cat json.txt | jq '.location.city'
+ if [[$UPDATE = 'snapshot']]
+ cat json.txt | jq '.latest.snapshot'
  }
