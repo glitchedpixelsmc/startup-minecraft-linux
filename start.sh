5034c0b7fb4ebafbcc9fad14705782905207d902
kylerschin <kylerschin@gmail.com>
2016-02-24 09:26:44 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,10 +18,11 @@ timelimit=10
 #TimeLimit in seconds before restart. Press any key inbetween timelimit to stop server.
 updatestart='true'
 #MINHEAP option s MAXHEAP option x and options o
-DFTBA='TRUE'" >> conf.sh
+DFTBA='TRUE'" > conf.sh
 fi
 echo 'Reading Configuration File'
 ./conf.sh
+./currentversion.sh
 echo 'Done Reading Configuration File'
 echo 'Configuring Functions'
 while getopts rxscu option do case "${option}" in r) RESTARTCRASHtemp=${OPTARG};; x) MAXHEAPtemp=${OPTARG};; s) MINHEAPtemp=${OPTARG};; o) OPTIONStemp=${OPTARG};; esac done
@@ -55,17 +56,23 @@ fi
   ME=`whoami`
  echo 'Done Configurating Functions'
  java -version
- core () {java -Xmx${MAXHEAP} -Xms${MINHEAP} -XX:+UseConcMarkSweepGC \
+ core () {if [[$ENABLEUPDATE = 'true']]
+ then
+ update
+ fi
+ java -Xmx${MAXHEAP} -Xms${MINHEAP} -XX:+UseConcMarkSweepGC \
  -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=$CPU_COUNT -XX:+AggressiveOpts \
  -jar $SERVICE $OPTIONS
  }
-start () { echo 'Starting the server...'&& cd $DIR && core
- if [[$RESTARTCRASH = 'true']]
- then
- echo -e " You have $timelimit seconds\n Stop it quickly or It will Restart... \c"
+startloop () { 
+echo 'Starting the server...'
+ while [ condition ]
+do
+core
+	echo -e " You have $timelimit seconds\n Stop it quickly or It will Restart... \c"
 stop=""
 read -t $timelimit stop
-#read -t $timelimit name <&1 
+#read -t $timelimit name <&1
 # for bash versions bellow 3.x
 if [ ! -z "$stop" ]
 then
@@ -73,18 +80,43 @@ echo -e "Stopping..."
 exit
 else
 echo -e "\n TIME OUT\n Restarting Server..."
-core
 fi 
- fi}
+done
+ }
+start () {
+if [[$RESTARTCRASH = 'true']] then
+cd $DIR
+startloop
+else
+cd $DIR
+core
+fi
+}
  update () { echo 'fetching version data...'
  wget https://launchermeta.mojang.com/mc/game/version_manifest.json -O json.txt
+ $a='http://s3.amazonaws.com/Minecraft.Download/versions/'
+ $b'/minecraft_server.'
+ $c='.jar'
  echo 'Reading Json data...'
  if [[$UPDATE = 'snapshot']]
  then
- cat json.txt | jq '.latest.snapshot'
+ version=$(cat json.txt | jq '.latest.snapshot')
+ if [[$currentversion = $version]]
+ then
+ else
+ wget $a$version$b$version$c -O $SERVICE
+ echo 'currentversion='$version > 'currentversion.sh'
+ fi
  fi
  if [[$UPDATE = 'release']]
  then
- wget -O $SERVICE
+ version=$(cat json.txt | jq '.latest.release')
+ if [[$currentversion = $version]]
+ then
+ else
+ wget $a$version$b$version$c -O $SERVICE
+ echo 'currentversion='$version > 'currentversion.sh'
+ fi
  fi
  }
+ start
